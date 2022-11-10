c8006d1c4d8efbb8167e045e56efecedb450b1a9
tobipristupin <tobipristupin@gmail.com>
2022-11-09 21:57:16 -0600

    - Added test for parsing cmd events

--- PmuEvent.cpp
+++ PmuEvent.cpp
@@ -3,7 +3,7 @@
 PmuEvent::PmuEvent(std::string name, PmuEvent::Type type, std::string modifiers) : name(std::move(name)), type(type), modifiers(std::move(modifiers)) {}
 
 bool PmuEvent::operator==(const PmuEvent &e) const {
-    return (this->name == e.name) && (this->modifiers == e.modifiers);
+    return (this->name == e.name) && (this->modifiers == e.modifiers) && (this->type == e.type);
 }
 
 
--- test/PmuEventParserTest.cpp
+++ test/PmuEventParserTest.cpp
@@ -6,8 +6,15 @@
 
 TEST(PmuEventParserTest, parseSoftwareEvents){
     std::string eventsCmdInput = "branches,branch-misses:u,instructions:uv,bpf-output,duration_time,L1-dcache-loads";
-    std::vector<PmuEvent> events = parseEvents(eventsCmdInput);
-    for (const PmuEvent &e : events){
-        std::cout << e.name << "\n";
-    }
+    std::vector<PmuEvent> parsedEvents = parseEvents(eventsCmdInput);
+    std::vector<PmuEvent> expectedEvents = {
+            {"branches", PmuEvent::Type::HARDWARE, ""},
+            {"branch-misses", PmuEvent::Type::HARDWARE, "u"},
+            {"instructions", PmuEvent::Type::HARDWARE, "uv"},
+            {"bpf-output", PmuEvent::Type::SOFTWARE, ""},
+            {"duration_time", PmuEvent::Type::SOFTWARE, ""},
+            {"L1-dcache-loads", PmuEvent::Type::HARDWARE, ""},
+    };
+
+    ASSERT_EQ(parsedEvents, expectedEvents);
 }
