3282c97edd02045f385368f26f6eb8cf9e5f2077
tobipristupin <tobipristupin@gmail.com>
2022-11-16 00:38:45 -0600

    - Added standalone cpp file to run isolated cpp code for testing

---
+++ test/standalone.cpp
@@ -0,0 +1,7 @@
+//standalone file to test cpp code. This file compilers to its own executable that does not touch any bayesperf code.
+
+#include <iostream>
+
+int main(int argc, char **argv){
+    return 0;
+}
